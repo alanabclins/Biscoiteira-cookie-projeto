1e2e6c1e21ad329b9c5f8cda571bdfaa0e2599bd
alanabclins <alanacamaralins@gmail.com>
2023-04-20 11:57:04 -0300

    Fabrica

--- src/Fabrica.java
+++ src/Fabrica.java
@@ -1,3 +1,23 @@
-public class Fabrica {
+public class Fabrica  extends Produto{
+    private int fornada;
+    private int estoque;
+    public int getFornada() {
+        return fornada;
+    }
+    public void setFornada(int fornada) {
+        this.fornada = fornada;
+    }
+    public int getEstoque() {
+        return estoque;
+    }
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    public Fabrica(String nome, double preco, int quantidade, int fornada, int estoque) {
+        super(nome, preco, quantidade);
+        this.fornada = fornada;
+        this.estoque = quantidade;
+    }
+    
     
 }
