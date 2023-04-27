083b39acc4d042dc8752fa8c7465df51db92ffc3
dudagomesb <eduardagomesbarros@outlook.com>
2023-04-27 12:02:04 -0300

    fix

--- src/Clientes.java
+++ src/Clientes.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Clientes {
-    //List<Cadastro> listaCadastros = new ArrayList<>();
     public static List<Cadastro> listaCadastros = new ArrayList<Cadastro>();
     String nome;
     String telefone;
